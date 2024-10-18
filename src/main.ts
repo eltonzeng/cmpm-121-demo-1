3a84940e5d831399ba027979ff843039a22668a1
Elton Zeng <ezeng2@ucsc.edu>
2024-10-17 22:09:34 -0700

    step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,20 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Not my amazing game";
 document.title = gameName;
 
-
 const button = document.createElement("button");
 button.textContent = "Don't click me 😛";
-button.style.backgroundColor = "blue"
-button.style.color = 'white'
+button.style.backgroundColor = "blue";
+button.style.color = "white";
 document.body.appendChild(button);
 
 let counter: number = 0;
 const counterDiv = document.createElement("div");
-counterDiv.textContent = `${counter} mangoes`; // Update content with counter 
+counterDiv.textContent = `${counter} mangoes`; // Update content with counter
 app.append(counterDiv); // Add counter div to app
 
 button.addEventListener("click", () => {
   counter++; // Increase counter on click
-  counterDiv.textContent = `${counter} mangoes`; // Update counter 
+  counterDiv.textContent = `${counter} mangoes`; // Update counter
 });
 
 const header = document.createElement("h1");
