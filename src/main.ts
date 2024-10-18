6b58998c702a1ca522eda771fc77586833e1c4bd
Elton Zeng <ezeng2@ucsc.edu>
2024-10-17 22:01:30 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Not my amazing game";
 document.title = gameName;
 
+const button = document.createElement('button');
+button.textContent = "Don't click me 😛";
+document.body.appendChild(button);
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
